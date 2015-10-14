92cc2214e2a94492b2a1667ae77f95fb05b81bdb
Your Name <you@example.com>
2015-10-14 21:20:05 +0100

    1st commit

---
+++ gtk2notify.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'gtk2notify'
+  s.version = '0.1.0'
+  s.summary = 'Similar in functionality to libnotify'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/gtk2notify.rb']
+  s.add_runtime_dependency('gtk2', '~> 3.0', '>=3.0.7')
+  s.signing_key = '../privatekeys/gtk2notify.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/gtk2notify'
+end
---
+++ lib/gtk2notify.rb
@@ -0,0 +1,37 @@
+#! /usr/bin/env ruby
+
+# file: gtk2notify.rb
+
+require 'gtk2'
+
+class GtkNotify
+
+  def self.show(body: 'message body goes here', summary: '', 
+                                        timeout: 2.5, offset: {})
+
+    offset = {x: 30, y: 10}.merge offset
+
+    label = Gtk::Label.new(body)
+    window = Gtk::Window.new
+    window.add(label).show_all
+
+    window.decorated = false
+    window.border_width = 10
+
+    window.gravity = Gdk::Window::GRAVITY_NORTH_EAST
+    width, height =  window.size
+    x,y = offset[:x], offset[:y]
+    window.move(Gdk.screen_width - (width + x), y)
+
+    window.show_all
+    Thread.new {sleep timeout; window.hide_all; Gtk.main_quit}
+
+    Gtk.main
+  end
+end
+
+if __FILE__ == $0 then
+
+  GtkNotify.show body: 'Testing the notification system', offset: {x: 120} 
+
+end
